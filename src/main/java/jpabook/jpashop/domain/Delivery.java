605d963448425fdf98cb90c3e127686f8de6939e
seilylook <seilylook@naver.com>
2020-11-29 20:25:47 +0900

    third commit

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING) // 이거 ordinal로 안쓰는거 조심하자잉
+    private DeliveryStatus status;
+}
