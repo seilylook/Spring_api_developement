7d2408f26230cb9f9d4ee95eac059ac93d995b2f
seilylook <seilylook@naver.com>
2021-01-03 20:31:59 +0900

    23 commit

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -16,12 +16,12 @@ public class OrderQueryRepository {
     public List<OrderQueryDto> findOrderQueryDtos() { //루트 조회(toOne 코드를 모두 한번에 조회)
         List<OrderQueryDto> result = findOrders();
         result.forEach(o -> {
-        List<OrderItemQueryDto> orderItems =
-                findOrderItems(o.getOrderId());
-        o.setOrderItems(orderItems);
-    });
+            List<OrderItemQueryDto> orderItems =
+                    findOrderItems(o.getOrderId());
+            o.setOrderItems(orderItems);
+        });
         return result;
-}
+    }
 
     /**
      * 1:N 관계(컬렉션)를 제외한 나머지를 한번에 조회
@@ -29,7 +29,7 @@ public class OrderQueryRepository {
     private List<OrderQueryDto> findOrders() {
         return em.createQuery(
                 "select new jpabook.jpashop.repository.order.query.OrderQueryDto(o.id, m.name, o.orderDate, o.status, d.address)" +
-                " from Order o" +
+                        " from Order o" +
                         " join o.member m" +
                         " join o.delivery d", OrderQueryDto.class)
                 .getResultList();
@@ -47,7 +47,7 @@ public class OrderQueryRepository {
                 "select new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id,i.name, oi.orderPrice,oi.count)" +
                         " from OrderItem oi" +
                         " join oi.item i" +
-                        " where oi.order.id in : orderIds", OrderItemQueryDto.class)
+                        " where oi.order.id in :orderIds", OrderItemQueryDto.class)
                 .setParameter("orderIds", orderIds)
                 .getResultList();
 
@@ -59,32 +59,18 @@ public class OrderQueryRepository {
         return result;
     }
 
-    public List<OrderFlatDto> findAllByDto_flat() {
+    /**
+     * 1:N 관계인 orderItems 조회
+     */
+    private List<OrderItemQueryDto> findOrderItems(Long orderId) {
         return em.createQuery(
-                "select new
-                jpabook.jpashop.repository.order.query.OrderFlatDto(o.id, m.name, o.orderDate,
-                        o.status, d.address, i.name, oi.orderPrice, oi.count)" +
-    }
-        " from Order o" +
-                " join o.member m" +
-                " join o.delivery d" +
-                " join o.orderItems oi" +
-                " join oi.item i", OrderFlatDto.class)
-            .getResultList();
-
-
-/**
- * 1:N 관계인 orderItems 조회
- */
-    private List<OrderItemQueryDto> findOrderItems(Long orderId){
-            return em.createQuery(
-            "select new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id,i.name, oi.orderPrice,oi.count)" +
-            " from OrderItem oi"+
-            " join oi.item i"+
-            " where oi.order.id = : orderId",
-            OrderItemQueryDto.class)
-            .setParameter("orderId",orderId)
-            .getResultList();
+                "select new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id,i.name, oi.orderPrice,oi.count)" +
+                        " from OrderItem oi" +
+                        " join oi.item i" +
+                        " where oi.order.id = : orderId",
+                OrderItemQueryDto.class)
+                .setParameter("orderId", orderId)
+                .getResultList();
     }
 
 }
