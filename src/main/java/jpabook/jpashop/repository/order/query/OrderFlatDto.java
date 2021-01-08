cfb2ba0ebb2db17e325a5b2c36eac8ad5d6d4c0f
seilylook <seilylook@naver.com>
2021-01-08 18:51:54 +0900

    27 commit

--- src/main/java/jpabook/jpashop/repository/order/query/OrderFlatDto.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderFlatDto.java
@@ -13,16 +13,21 @@ public class OrderFlatDto {
     private Long orderId;
     private String name;
     private LocalDateTime orderDate;
-    private OrderStatus orderStatus;
     private Address address;
-    private List<OrderItemQueryDto> orderItems;
+    private OrderStatus orderStatus;
 
-    public OrderFlatDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address, List<OrderItemQueryDto> orderItems) {
+    private String itemName;
+    private int orderPrice;
+    private int count;
+
+    public OrderFlatDto(Long orderId, String name, LocalDateTime orderDate, Address address, OrderStatus orderStatus, String itemName, int orderPrice, int count) {
         this.orderId = orderId;
         this.name = name;
         this.orderDate = orderDate;
-        this.orderStatus = orderStatus;
         this.address = address;
-        this.orderItems = orderItems;
+        this.orderStatus = orderStatus;
+        this.itemName = itemName;
+        this.orderPrice = orderPrice;
+        this.count = count;
     }
 }
